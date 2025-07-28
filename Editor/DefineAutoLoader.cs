6c8a550ff405c9df410cd13d92125a3748359319
Iskald <>
2025-07-28 16:00:45 +0200

    added define auto loader

---
+++ Editor/DefineAutoLoader.cs
@@ -0,0 +1,23 @@
+﻿namespace IceCold.Editor
+{
+    using UnityEditor;
+    
+    [InitializeOnLoad]
+    public static class DefineAutoLoader
+    {
+        private const string DefineSymbol = "ICE_COLD";
+        
+        static DefineAutoLoader()
+        {
+            var namedBuildTarget = UnityEditor.Build.NamedBuildTarget.FromBuildTargetGroup(EditorUserBuildSettings.selectedBuildTargetGroup);
+            var defines = PlayerSettings.GetScriptingDefineSymbols(namedBuildTarget);
+
+            if (defines.Contains(DefineSymbol)) return;
+            
+            if (!string.IsNullOrEmpty(defines) && !defines.EndsWith(";"))
+                defines += ";";
+            defines += DefineSymbol;
+            PlayerSettings.SetScriptingDefineSymbols(namedBuildTarget, defines);
+        }
+    }
+}
